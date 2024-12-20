5e7b967b61500370912467638ca6d3a6e02ced5b
maddiodie <maddiodie@gmail.com>
2024-12-19 23:57:58 -0700

    finished 'review technical foundation: big o notation'

---
+++ src/main/java/org/example/BigONotation.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class BigONotation {
+
+    
+
+    // NOTES:
+    // - measures time and space complexities of algorithms
+    // - time complexity -> number of operations per input
+    // - space complexity -> number of variables in memory per input
+    // O(n) is visiting every single one of the elements
+    // the o in big o stands for order of magnitude
+    // we avoid algorithms that have very large complexities
+    // sometimes to improve the time complexity we might need to create an auxiliary data structure
+    //  this means that to achieve a better time complexity for an algorithm, you might need to use
+    //  an additional data structure, which could help optimize the operations and reduce the overal
+    //  time complexity
+
+}
